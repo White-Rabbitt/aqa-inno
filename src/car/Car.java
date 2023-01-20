aca6ffaee95fc6be8e6026548eadf4b22ab1f5e1
White-Rabbitt <wr989@mail.ru>
2023-01-20 15:22:19 +0300

    Add Car, Cat

--- src/car/Car.java
+++ src/car/Car.java
@@ -1,7 +1,30 @@
 package car;
 
 public class Car {
-    public static void main(String[] args) {
+    String color;
+    String type;
+    int year;
 
+    public void start() {
+        System.out.println("Автомобиль заведен");
+    }
+
+    public void stop() {
+        System.out.println("Автомобиль заглушен");
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public Car() {
     }
 }
---
+++ src/pet/Cat.java
@@ -0,0 +1,10 @@
+package pet;
+
+public class Cat extends Animal {
+    public Cat() {
+    }
+
+    public void drinkMilk() {
+        System.out.println("Кошка пьёт воду");
+    }
+}
